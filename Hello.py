e324f8caf9fdf076247cde583fa1076cd70361a1
fiticosmica <154455286+fiticosmica@users.noreply.github.com>
2024-03-07 11:34:25 -0300

    logo update

--- Hello.py
+++ Hello.py
@@ -1,13 +1,17 @@
 import streamlit as st
+from PIL import Image
+import pandas as pd
 from streamlit.logger import get_logger
 
 LOGGER = get_logger(__name__)
+img_ctci = Image.open("/media/marca.png")
 
 def run():
     st.set_page_config(
         page_title="Nodo CTCI",
         page_icon="🧬",
     )
+    st.image(img_ctci, width=200)
     st.title("CTCI Nodo Centro Sur")
     st.write("El objetivo general es co-crear un modelo de Ciencia Abierta para fortalecer el desarrollo de la ciencia y tecnología en la Macrozona Centro Sur de Chile en concordancia con su territorio y sociedad.")
     st.write("---")
