3b2616f3b49df4536dcf11ade59a5b0a1b4747f2
fiticosmica <154455286+fiticosmica@users.noreply.github.com>
2024-03-07 11:48:30 -0300

    Update 3_Demo.py

--- pages/3_Demo.py
+++ pages/3_Demo.py
@@ -24,8 +24,8 @@ from streamlit.hello.utils import show_code
 def data_frame_demo():
     @st.cache_data
     def get_UN_data():
-        #AWS_BUCKET_URL = "https://streamlit-demo-data.s3-us-west-2.amazonaws.com"
-        df = pd.read_csv(data.csv)
+        AWS_BUCKET_URL = "https://streamlit-demo-data.s3-us-west-2.amazonaws.com"
+        df = pd.read_csv("data.csv")
         return df.set_index("Region")
 
     try:
