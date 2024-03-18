27af2a3208d28769f910c3ec1da9d1b23030153d
fiticosmica <154455286+fiticosmica@users.noreply.github.com>
2024-03-18 11:15:21 -0300

    Update Hello.py

--- Hello.py
+++ Hello.py
@@ -1,11 +1,11 @@
 import streamlit as st
 from PIL import Image
 import pandas as pd
+import plotly.express as px
 from streamlit.logger import get_logger
 
 LOGGER = get_logger(__name__)
 img_ctci = Image.open("media/marca.png")
-#df = pd.read_csv('data.csv')
 
 def run():
     st.set_page_config(
@@ -17,10 +17,11 @@ def run():
     st.write("El objetivo general es co-crear un modelo de Ciencia Abierta para fortalecer el desarrollo de la ciencia y tecnología en la Macrozona Centro Sur de Chile en concordancia con su territorio y sociedad.")
     st.write("---")
 
-    db_excel = 'datos.xlsx'
+    archivo_excel = pd.ExcelFile('datos.xlsx')
     hoja_exel = 'Hoja1'
     
-    df = pd.read_excel(db_excel, sheet_name=hoja_exel, usecols='A:N',header=0)
+    df = pd.read_excel(archivo_excel,sheet_name=hoja_exel,usecols='A:N',header=0)
+    
     df_iniciativas= df.groupby(['Macrotema'], as_index=False)
     
     st.write(df_iniciativas)
