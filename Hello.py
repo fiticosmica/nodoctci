2990c6f11c8020356f5ebf843a223c410fb8afef
fiticosmica <154455286+fiticosmica@users.noreply.github.com>
2024-03-18 11:22:25 -0300

    Update Hello.py

--- Hello.py
+++ Hello.py
@@ -16,7 +16,7 @@ def run():
     st.write("El objetivo general es co-crear un modelo de Ciencia Abierta para fortalecer el desarrollo de la ciencia y tecnología en la Macrozona Centro Sur de Chile en concordancia con su territorio y sociedad.")
     st.write("---")
 
-    archivo_excel = pd.ExcelFile('datos.xlsx')
+    archivo_excel = pd.ExcelFile('/datos.xlsx')
     hoja_exel = 'Hoja1'
     
     #df = pd.read_excel(archivo_excel,sheet_name=hoja_exel,usecols='A:N',header=0)
